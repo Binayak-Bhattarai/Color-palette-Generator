e18e20c9675cec871fde252ec7fda81d200c887f
Binayak-Bhattarai <binayakbhattaraiofficial.99@gmail.com>
2025-01-28 16:02:12 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+function randomHexColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function randomRgbColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function generateBox(){
+const num = parseInt(document.getElementById('num').value);
+const format=document.getElementById("format").value;
+const boxContainer= document.getElementById("boxcontainer");
+
+if (isNaN(num) || num < 1) {
+    alert("Please enter a valid number of colors.");
+    return;
+}
+boxContainer.innerHTML = "";
+
+
+for (let i = 0; i < num; i++) {
+    let color = format === "HEX" ? randomHexColor() : randomRgbColor();
+    let box = document.createElement("div");
+    box.className = "box";
+    boxContainer.appendChild(box);
+    box.style.backgroundColor = color;
+
+    let input = document.createElement("input");
+    input.type="text";
+    input.id= "clrcode";
+    box.appendChild(input);
+    input.value = color;
+    
+
+  
+}
+
+}
+function clearBox() {
+    document.getElementById("boxcontainer").innerHTML = "";
+}
